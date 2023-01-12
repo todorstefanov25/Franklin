069151cdc83aad9a064cd6fc997c5b8948f37ff5
Ivelina <daniel_botev2004@abv.bg>
2023-01-12 11:39:50 +0200

    git push

---
+++ oshte franklin i nedqlko/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace oshte_franklin_i_nedqlko
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var currentNum = 1;
+
+            int sum = 1;
+            for (int row = 1; row <= n; row++)
+            {
+                for (int col = 1; col <= row; col++)
+                {
+                    Console.Write(sum + " ");
+                    sum++;
+                    if (sum > n)
+                    {
+                        return;
+                    }
+                }
+                Console.WriteLine();
+            }    }
+    }
+}
